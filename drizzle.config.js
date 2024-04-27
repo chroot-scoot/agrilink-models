5f31b10899bdfa65730ad4019adcb111dbfbdad9
Abhinav Pandey <abhinav.pandey.1512@gmail.com>
2024-04-27 17:52:11 +0530

    init

---
+++ drizzle.config.js
@@ -0,0 +1,11 @@
+/** @type { import("drizzle-kit").Config } */
+const config = {
+  schema: "./models",
+  out: "./migrations",
+  driver: "pg",
+  dbCredentials: {
+    connectionString: process.env.DB_URL,
+  },
+};
+
+export default config;
